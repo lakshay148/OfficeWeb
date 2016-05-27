6ed2f3691f9ff4374254f6af20e6be32d9d1ecd9
lakshay girdhar <lakshay.girdhar@gaadi.com>
2016-05-27 13:52:03 +0530

    Added User Registration Apis

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,22 +1,26 @@
 from rest_framework import serializers
 from apis.models import User
 
-class UserSerializer(serializers.Serializer):
-	name = serializers.CharField(required=True,max_length=100)
-	password = serializers.CharField(required=True,max_length=256)
-	phone = serializers.CharField(required=True,max_length=11)
-	email = serializers.CharField(required=True,max_length=100)
-	role = serializers.CharField(required=True)
+# class UserSerializer(serializers.Serializer):
+# 	name = serializers.CharField(required=True,max_length=100)
+# 	password = serializers.CharField(required=True,max_length=256)
+# 	phone = serializers.CharField(required=True,max_length=11)
+# 	email = serializers.CharField(required=True,max_length=100)
+# 	role = serializers.CharField(required=True)
 
-	def create(self,validated_data):
-		return User.objects.create(**validated_data)
+# 	def create(self,validated_data):
+# 		return User.objects.create(**validated_data)
 
-	def update(self,instance,validated_data):
-		instance.name = validated_data.get('name',instance.name)
-		instance.password = validated_data.get('password',instance.password)
-		instance.phone = validated_data.get('phone', instance.phone)
-		instance.role = validated_data.get('role', instance.role)
-		instance.email = validated_data.get('email',instance.email)
-		return instance
+# 	def update(self,instance,validated_data):
+# 		instance.name = validated_data.get('name',instance.name)
+# 		instance.password = validated_data.get('password',instance.password)
+# 		instance.phone = validated_data.get('phone', instance.phone)
+# 		instance.role = validated_data.get('role', instance.role)
+# 		instance.email = validated_data.get('email',instance.email)
+# 		return instance
 
-
+class UserSerializer(serializers.ModelSerializer):
+	"""docstring for UserSerializer"""
+	class Meta:
+		model = User	
+		fields = ('id','name','password','phone','email','role')
--- apis/views.py
+++ apis/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from serializers import UserSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+@csrf_exempt
+def users(request):
+	if request.method == 'POST' :
+		data = JSONParser().parse(request)
+		serializer = UserSerializer(data = data)
+		if serializer.is_valid():
+			serializer.save()
+			return JSONResponse(serializer.data, status=201)
+		else: 
+			return JSONResponse(serializer.errors,status = 400)
+	return HttpResponse(status=400)
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+
+
+
